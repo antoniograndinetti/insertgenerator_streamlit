63082c9744eeb034b5827b7c4032d7456f862b48
Antonio Grandinetti <antoniograndinetti91@gmail.com>
2024-10-29 16:15:46 +0100

    change layout

--- ui_main.py
+++ ui_main.py
@@ -7,28 +7,95 @@ st.set_page_config(layout="wide")
 # Titolo della pagina
 st.title("Winday sms plan insert generator")
 
+# Nome del file di output
+output_file = 'output.sql'
+
+upper_col1, upper_col2 = st.columns(2)
+
 # Selettori di date
 # Disposizione dei selettori di date su una riga
-col1, col2 = st.columns(2)
+col1, col2 = upper_col1.columns(2)
 with col1:
     start_date = st.date_input("Data di inizio", value=date.today())
 with col2:
     end_date = st.date_input("Data di fine", value=date.today())
 
-# Carica il file
-uploaded_file = st.file_uploader("Carica un file di testo", type=["xlsx", "xls"])
+# Controlla se i risultati sono già presenti nello stato della sessione
+if "results_displayed" not in st.session_state:
+    st.session_state["results_displayed"] = False
 
-if st.button("Conta le righe"):
-    if uploaded_file is not None:
-        # Lettura del file
-        try:
-            wgi.generate_insert(uploaded_file, start_date, end_date, st)
-            
-            # Sezione per mostrare o nascondere testo aggiuntivo
-            # show_more = st.checkbox("Mostra/nascondi testo aggiuntivo")
-            # if show_more:
-            #     st.write("Questo è il testo aggiuntivo che può essere visualizzato o nascosto.")
-        except Exception as e:
-            st.write("Errore nella lettura del file:", e)
-    else:
-        st.write("Carica un file da elaborare.")
+# Carica il file
+uploaded_file = upper_col1.file_uploader("Carica il file del piano", type=["xlsx", "xls"])
+
+but_col1, but_col2, _ = upper_col1.columns([1,1,7])
+with but_col1:
+    if st.button("Run", key="generate", type="primary"):
+        if uploaded_file is not None:
+            try:
+                wgi.generate_insert(uploaded_file, output_file, start_date, end_date, st)
+                
+            except Exception as e:
+                st.write("Errore nella lettura del file:", e)
+        else:
+            st.write("Carica un file da elaborare.")
+
+with but_col2:
+    if st.button("Clear all", key="clear"):
+        st.session_state.clear()
+        st.session_state["results_displayed"] = False
+
+res_col1, res_col2 = st.columns(2)
+
+with res_col1:
+    if st.session_state["results_displayed"]:
+        st.markdown(st.session_state["log_content"], unsafe_allow_html=True)
+
+with upper_col2:
+    if st.session_state["results_displayed"]:
+        
+        # Mostra le statistiche con colori e formattazione
+        st.markdown(f"""
+    <div style="border: 1px solid #ddd; padding: 16px; border-radius: 8px;">
+        <h3 style="color: #4CAF50;">STATISTICS:</h3>
+        <table style="width: 100%; border-collapse: collapse;">
+            <tr>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Date selezionate:</td>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">inizio {start_date} - fine {end_date}</td>
+            </tr>
+            <tr>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Le insert sono state salvate nel file:</td>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{output_file}</td>
+            </tr>
+            <tr>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Giorni generati:</td>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{st.session_state["counter_days"]}</td>
+            </tr>
+            <tr>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Insert nella tabella campaign:</td>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{st.session_state["counter_insert_campaign"]}</td>
+            </tr>
+            <tr>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Insert nella tabella di ampliamento:</td>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{st.session_state["counter_insert_ampliamento"]}</td>
+            </tr>
+            <tr>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Insert nella sms_config:</td>
+                <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{st.session_state["counter_insert_subcluster"]}</td>
+            </tr>
+            <tr>
+                <td style="color: #FF0000; padding: 8px; border-bottom: 1px solid #ddd;">Warning:</td>
+                <td style="color: #FF0000; padding: 8px; border-bottom: 1px solid #ddd;">{st.session_state["counter_warning"]}</td>
+            </tr>
+        </table>
+    </div>
+    """, unsafe_allow_html=True)
+    
+
+if st.session_state["results_displayed"]:
+    with open(output_file, "rb") as file:
+        st.download_button(
+            label="Scarica il file SQL",
+            data=file,
+            file_name=output_file,
+            mime="application/sql"
+        )
--- windtre_generate_insert.py
+++ windtre_generate_insert.py
@@ -1,10 +1,7 @@
-from datetime import datetime
+# from datetime import datetime
 import pandas as pd
 import unicodedata
 import re
-
-# Nome del file di output
-output_file = 'output.sql'
 
 # Funzione per convertire il giorno in formato abbreviato
 def abbreviazione_giorno(giorno):
@@ -46,52 +43,49 @@ def is_string_different(original, transformed):
         if i >= len(transformed) or char != transformed[i]:
             removed_chars.append(char)
     return len(removed_chars) != 0
+    
+def generate_insert(file_path, output_file, data_inizio, data_fine, st):
 
-def generate_insert(file_path, data_inizio, data_fine, st):
+    log_content ="""
+        <div style="border: 1px solid #ddd; padding: 16px; border-radius: 8px;">
+            <h3 style="color: #4CAF50;">LOG:</h3>"""
 
-    col1, col2 = st.columns(2)
+    # Log di informazioni nella colonna sinistra
+    log_content += "<div style='color: #000; font-weight: bold;'>INFO: Inizio lettura file...</div>"
 
-    with col1:
-        log_content ="""
-            <div style="border: 1px solid #ddd; padding: 16px; border-radius: 8px;">
-                <h3 style="color: #4CAF50;">LOG:</h3>"""
+    # CONFIGURAZIONI
+    # file_path = 'Piano_WINDAY_All5.xlsx' # Specifica il nome del excel qui
+    sheet_name = 'Testi SMS'  # Specifica il nome del foglio qui
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
 
-        # Log di informazioni nella colonna sinistra
-        log_content += "<div style='color: #000; font-weight: bold;'>INFO: Inizio lettura file...</div>"
+    counter_days = 0
+    counter_insert_campaign = 0
+    counter_insert_ampliamento = 0
+    counter_insert_subcluster = 0
+    counter_warning = 0
 
-        # CONFIGURAZIONI
-        # file_path = 'Piano_WINDAY_All5.xlsx' # Specifica il nome del excel qui
-        sheet_name = 'Testi SMS'  # Specifica il nome del foglio qui
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
+    with open(output_file, mode='w') as output:
+        # reader = csv.DictReader(file, delimiter=';')
+        for index, row in df.iterrows():
+            data_dt = row['Data']
+            if data_dt != None and pd.isnull(data_dt) == False:
+                data_str = data_dt.strftime('%Y-%m-%d')
 
-        counter_days = 0
-        counter_insert_campaign = 0
-        counter_insert_ampliamento = 0
-        counter_insert_subcluster = 0
-        counter_warning = 0
+                # Filtra le righe in base alle date
+                if pd.to_datetime(data_inizio) <= data_dt and data_dt <= pd.to_datetime(data_fine):
+                    counter_days = counter_days + 1
 
-        with open(output_file, mode='w') as output:
-            # reader = csv.DictReader(file, delimiter=';')
-            for index, row in df.iterrows():
-                data_dt = row['Data']
-                if data_dt != None and pd.isnull(data_dt) == False:
-                    data_str = data_dt.strftime('%Y-%m-%d')
-
-                    # Filtra le righe in base alle date
-                    if pd.to_datetime(data_inizio) <= data_dt and data_dt <= pd.to_datetime(data_fine):
-                        counter_days = counter_days + 1
-
-                        output.write(f"""
+                    output.write(f"""
 ------------------ INIZIO data {data_str} --------------------""")
-                        giorno = row['Giorno']
-                        log_content += f"<div style='color: #000;'>INFO: Generando per giorno {giorno}, data {data_str}.</div>"
+                    giorno = row['Giorno']
+                    log_content += f"<div style='color: #000;'>INFO: Generando per giorno {giorno}, data {data_str}.</div>"
 
-                        note = row['Note']
-                        ampliamento = 'S' if note == 'DA PREVEDERE AMPLIAMENTO SU CLUSTER OLD E SUPEROLD' else 'N'
-                        giorno_abbr = abbreviazione_giorno(giorno)
+                    note = row['Note']
+                    ampliamento = 'S' if note == 'DA PREVEDERE AMPLIAMENTO SU CLUSTER OLD E SUPEROLD' else 'N'
+                    giorno_abbr = abbreviazione_giorno(giorno)
 
-                        # Inserimento per la tabella campaign
-                        campaign_insert = f"""
+                    # Inserimento per la tabella campaign
+                    campaign_insert = f"""
 INSERT INTO public.campaign
 (code, campaign_channel, campaign_type, enddt, fl_imported, name, startdt, day, creation_date, contact_dt, file_name, fl_exported_selligent, fl_has_lead, stack_brand)
 VALUES('MarketingAutomation{data_dt.strftime('%Y%m%d')}',
@@ -108,90 +102,59 @@ NULL,
 'N',
 'CLUSTER',
 NULL);
-        """
-                        output.write(campaign_insert)
-                        counter_insert_campaign = counter_insert_campaign + 1
+    """
+                    output.write(campaign_insert)
+                    counter_insert_campaign = counter_insert_campaign + 1
 
-                        sub_cluster_list = [14, 15, 16, 17, 18, 21]
+                    sub_cluster_list = [14, 15, 16, 17, 18, 21]
 
-                        # Inserimento su config_run_function se c'è ampliamento
-                        if ampliamento == 'S':
-                            config_run_function_insert = f"""
+                    # Inserimento su config_run_function se c'è ampliamento
+                    if ampliamento == 'S':
+                        config_run_function_insert = f"""
 INSERT INTO wind_cra_app.config_run_function
 (dt_start, dt_end, fl_active)
 VALUES('{data_str} 00:00:00.000',
 '{data_str} 00:00:00.000',
 'S');
-            """
-                            log_content += f"<div style='color: #000;'>INFO: Identificato ampliamento a old e superold.</div>"
-                            output.write(config_run_function_insert)
-                            counter_insert_ampliamento = counter_insert_ampliamento + 1
+        """
+                        log_content += f"<div style='color: #000;'>INFO: Identificato ampliamento a old e superold.</div>"
+                        output.write(config_run_function_insert)
+                        counter_insert_ampliamento = counter_insert_ampliamento + 1
 
-                            sub_cluster_list = [14, 15, 16, 17, 18, 19, 20, 21]
+                        sub_cluster_list = [14, 15, 16, 17, 18, 19, 20, 21]
 
-                        # Inserimento su sms_config
-                        sms_text_normalized = normalize_and_remove_non_ascii(row['Testo SMS  '])
+                    # Inserimento su sms_config
+                    sms_text_normalized = normalize_and_remove_non_ascii(row['Testo SMS  '])
 
-                        if is_string_different(row['Testo SMS  '], sms_text_normalized):
-                            output.write(f"""
------------------- WARN: CARATTERI ASCII TROVATI! VERIFICARE IL TESTO! --------------------\n""")
-                            log_content += f"""
-                                <div style='color: #FF9800; font-weight: bold;'>
-                                    WARN: Caratteri non ASCII rilevati nel giorno {data_str}. Verificare il testo!
-                                </div>
-                            """
-                            counter_warning += 1
-
-                        sms_text = sms_text_normalized.replace("'", "''")
-                        for id_cluster_priority in sub_cluster_list:
-                            sms_config_insert = insert_sms_config(id_cluster_priority, sms_text, data_str)
-                            counter_insert_subcluster += 1
-                            output.write(sms_config_insert)
-
+                    if is_string_different(row['Testo SMS  '], sms_text_normalized):
                         output.write(f"""
+------------------ WARN: CARATTERI ASCII TROVATI! VERIFICARE IL TESTO! --------------------\n""")
+                        log_content += f"""
+                            <div style='color: #FF9800; font-weight: bold;'>
+                                WARN: Caratteri non ASCII rilevati nel giorno {data_str}. Verificare il testo!
+                            </div>
+                        """
+                        counter_warning += 1
+
+                    sms_text = sms_text_normalized.replace("'", "''")
+                    for id_cluster_priority in sub_cluster_list:
+                        sms_config_insert = insert_sms_config(id_cluster_priority, sms_text, data_str)
+                        counter_insert_subcluster += 1
+                        output.write(sms_config_insert)
+
+                    output.write(f"""
 ------------------ FINE data {data_str} --------------------\n""")
 
-        log_content += "<div style='color: #000; font-weight: bold;'>INFO: Fine.</div>"
+    log_content += "<div style='color: #000; font-weight: bold;'>INFO: Fine.</div>"
         
-        # Chiudiamo il div del log box
-        log_content += "</div>"
+    # Chiudiamo il div del log box
+    log_content += "</div>"
 
-        st.markdown(log_content, unsafe_allow_html=True)
-
-    # Mostra le statistiche con colori e formattazione
-    with col2:
-        st.markdown(f"""
-        <div style="border: 1px solid #ddd; padding: 16px; border-radius: 8px;">
-            <h3 style="color: #4CAF50;">STATISTICS:</h3>
-            <table style="width: 100%; border-collapse: collapse;">
-                <tr>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Date selezionate:</td>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">inizio {data_inizio} - fine {data_fine}</td>
-                </tr>
-                <tr>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Le insert sono state salvate nel file:</td>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{output_file}</td>
-                </tr>
-                <tr>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Giorni generati:</td>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{counter_days}</td>
-                </tr>
-                <tr>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Insert nella tabella campaign:</td>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{counter_insert_campaign}</td>
-                </tr>
-                <tr>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Insert nella tabella di ampliamento:</td>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{counter_insert_ampliamento}</td>
-                </tr>
-                <tr>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">Insert nella sms_config:</td>
-                    <td style="color: #000; padding: 8px; border-bottom: 1px solid #ddd;">{counter_insert_subcluster}</td>
-                </tr>
-                <tr>
-                    <td style="color: #FF0000; padding: 8px; border-bottom: 1px solid #ddd;">Warning:</td>
-                    <td style="color: #FF0000; padding: 8px; border-bottom: 1px solid #ddd;">{counter_warning}</td>
-                </tr>
-            </table>
-        </div>
-        """, unsafe_allow_html=True)
+    st.session_state["log_content"] = log_content
+    st.session_state["counter_days"] = counter_days
+    st.session_state["counter_insert_campaign"] = counter_insert_campaign
+    st.session_state["counter_insert_ampliamento"] = counter_insert_ampliamento
+    st.session_state["counter_insert_subcluster"] = counter_insert_subcluster
+    st.session_state["counter_warning"] = counter_warning
+    st.session_state["results_displayed"] = True
+    
